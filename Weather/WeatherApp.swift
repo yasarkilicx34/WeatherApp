5d2f6f7f2294e36d5f7bb5a62ec66ba439ca1b8c
yasarkilicx <yasarkilicx@gmail.com>
2023-08-23 15:48:40 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by yasarkilic on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
